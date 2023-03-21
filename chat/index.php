510801f70886f711342b51e1b9dcb36a37213fb4
munzirulalom <cse.alomb008@gmail.com>
2023-03-21 21:43:59 +0600

    Remove all code and move to PHP_V1 old code.

--- chat/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  session_start();
-
-  if(isset($_SESSION['id'])){
-    header("location: users.php");
-  } else{
-    header("location: ". SITE_URL);
-  }
-?>
--- chat/login.php
+++
@@ -1,8 +0,0 @@
-<?php 
-  session_start();
-  include_once "php/config.php";
-  if(isset($_SESSION['id'])){
-    header("location: users.php");
-  }else{
-    header("location: ". SITE_URL);
-  }
